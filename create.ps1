3c8003a5353cbdb5cc53e3b4d3a8163f9b96eef5
rnieuweveen <78159491+rnieuweveen@users.noreply.github.com>
2024-01-15 13:46:52 +0100

    refactor connector (#1)
    $auditMessage > $auditLogs
    added name generation
    update changed with $sqlParameters; solves issue with single quotes in query

--- create.ps1
+++ create.ps1
@@ -5,70 +5,196 @@ $config = $configuration | ConvertFrom-Json;
 $database = $config.database
 $verbose = $config.verbose
 
-$success = $False;
-$auditMessage = "Account for person " + $p.DisplayName + " not created succesfully"
+$auditLogs = [Collections.Generic.List[PSCustomObject]]::new()
+
+function GenerateMiddleName {
+    [cmdletbinding()]
+    Param (
+        [object]$person
+    )
+    try {
+        $FamilyNamePrefix = $person.Name.FamilyNamePrefix 
+        $PartnerNamePrefix = $person.Name.FamilyNamePartnerPrefix
+        $convention = $person.Name.Convention
+
+        $middlename = ""
+        switch ($convention) {
+            "B" {
+                $middlename = $FamilyNamePrefix;
+            }
+            "P" {
+                $middlename = $PartnerNamePrefix;                 
+            }
+            "BP" {
+                $middlename = $FamilyNamePrefix;
+            }
+            "PB" {
+                $middlename = $PartnerNamePrefix;   
+            }
+            Default {
+                $middlename = $FamilyNamePrefix;
+            }
+        }
+
+        return $middlename
+    }
+    catch {
+        throw("An error was found in the name convention algorithm: $($_.Exception.Message): $($_.ScriptStackTrace)")
+    
+    }
+
+}
+function GenerateLastName {
+    [cmdletbinding()]
+    Param (
+        [object]$person
+    )
+
+    try {
+        $suffix = "";
+        $givenname = if ([string]::IsNullOrEmpty($person.Name.Nickname)) { $person.Name.Initials.Substring(0, 1) }else { $person.Name.Nickname }
+        $FamilyNamePrefix = $person.Name.FamilyNamePrefix
+        $FamilyName = $person.Name.FamilyName           
+        $PartnerNamePrefix = $person.Name.FamilyNamePartnerPrefix
+        $PartnerName = $person.Name.FamilyNamePartner 
+        $convention = $person.Name.Convention
+
+        $LastName = ""
+
+        switch ($convention) {
+            "B" {
+                #  $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName  
+            }
+            "P" {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $PartnerName                    
+            }
+            "BP" {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName + " - "
+                $LastName += if (-NOT([string]::IsNullOrEmpty($PartnerNamePrefix))) { $PartnerNamePrefix + " " }
+                $LastName += $PartnerName
+            }
+            "PB" {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($PartnerNamePrefix))) { $PartnerNamePrefix + " " }
+                $LastName += $PartnerName + " - "
+                $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName
+            }
+            Default {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName
+
+            }
+        }
+        return $LastName
+            
+    }
+    catch {
+        throw("An error was found in the name convention algorithm: $($_.Exception.Message): $($_.ScriptStackTrace)")
+    } 
+}
+
+switch ($p.details.gender) {
+    "Female" 
+    { $gender = "V" }
+    "Male" 
+    { $gender = "M" }
+    Default 
+    { $gender = "O" }
+}
 
 #Change mapping here
 $account = [PSCustomObject]@{
-    email      = $p.Accounts.MicrosoftActiveDirectory.mail;
-    voornamen  = $p.Name.NickName;
-    achternaam = $p.Custom.TOPdeskSurName;
-    adfs_login = $p.Accounts.MicrosoftActiveDirectory.SamAccountName;
-    externalId = $p.ExternalId;
+    email          = $p.Accounts.MicrosoftActiveDirectory.mail;
+    voornaam       = $p.Name.NickName
+    tussenvoegsel  = GenerateMiddleName -person $p
+    achternaam     = GenerateLastName -person $p
+    gebruikersnaam = $p.Accounts.MicrosoftActiveDirectory.SamAccountName
+    externalId     = $p.ExternalId
+    geslacht       = $gender
 }
 
 #correlation
 $correlationField = 'externalId'
 $correlationValue = $p.ExternalID
 
-Try {    
-    #Do not execute when running preview
-    if (-Not($dryRun -eq $True)) {
-        Import-Module PSSQLite       
-        If (!(Test-Path $database)) {
-            $query = "CREATE TABLE persons ($correlationField VARCHAR(100) PRIMARY KEY, email TEXT, achternaam TEXT, voornamen TEXT, adfs_login TEXT)"
-            Invoke-SqliteQuery -Query $query -DataSource $database -Verbose:$verbose
-            Write-Verbose -Verbose "Created new database $database with table name 'persons'"
-            if ($verbose) {
-                Invoke-SqliteQuery -DataSource $database -Query "PRAGMA table_info(persons)" -verbose:$verbose
-            }
+Try {     
+     
+    $query = "SELECT $correlationField,achternaam FROM persons WHERE $correlationField = '$correlationValue'"
+    $correlationCheckResult = Invoke-SqliteQuery -Query $query -DataSource $database -Verbose:$verbose
+
+
+    #Check correlation before create
+    if ($correlationCheckResult.externalId -eq $correlationValue) {
+        $aRef = ($correlationCheckResult.$correlationField)
+
+        $query = "UPDATE persons 
+                    SET email = @email
+                    ,achternaam = @achternaam
+                    ,voornaam = @voornaam
+                    ,tussenvoegsel = @tussenvoegsel
+                    ,gebruikersnaam = @gebruikersnaam
+                    ,geslacht = @geslacht
+                    ,createtime = datetime()
+                    WHERE externalId = @externalId"
+
+        $sqlParameters = $account.psobject.properties | ForEach-Object -begin { $h = @{} } -process { $h."$($_.Name)" = $_.Value } -end { $h }
+
+        if (-Not($dryRun -eq $True)) {
+            $null = Invoke-SqliteQuery -Query $query -DataSource $database -SqlParameters $sqlParameters -Verbose:$verbose
         }
-        
-        $query = "SELECT $correlationField FROM persons WHERE $correlationField = " + $correlationValue
-        $correlationCheckResult = Invoke-SqliteQuery -Query $query -DataSource $database -Verbose:$verbose
-      
-        #Check correlation before create
-        if ($correlationCheckResult.externalId -eq $correlationValue) {
-            $accountReference = ($correlationCheckResult.$correlationField)
-            $success = $True
-            $auditMessage = " with correlation for record with $($correlationField + " = " + $accountReference)"
-        } else {
-            $query = "INSERT OR REPLACE INTO persons ($correlationField, email, achternaam, voornamen, adfs_login) VALUES (@externalId,@email,@achternaam,@voornamen,@adfs_login);"
-            $sqlParameters = $account.psobject.properties | ForEach-Object -begin {$h=@{}} -process {$h."$($_.Name)" = $_.Value} -end {$h}
-            Invoke-SqliteQuery -DataSource $database -Query $query -SqlParameters $sqlParameters -Verbose:$verbose
-            $accountReference = $p.ExternalId
-            $success = $True
-            $auditMessage = " succesfully"
+        else {
+            write-warning "executing: $($query)"
+            write-warning "with parameters: $($sqlParameters | convertto-json)"
+
         }
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "CreateAccount"
+                Message = "Account correlated and updated for $($account.email) ($($account.$correlationField))"
+                IsError = $false
+            })  
+        $success = $true
+
+    }
+    else {
+        $query = "INSERT OR REPLACE INTO persons ($correlationField, email, achternaam, voornaam, tussenvoegsel, gebruikersnaam,geslacht,createtime) VALUES (@externalId,@email,@achternaam,@voornaam,@tussenvoegsel,@gebruikersnaam,@geslacht,datetime());"
+        $sqlParameters = $account.psobject.properties | ForEach-Object -begin { $h = @{} } -process { $h."$($_.Name)" = $_.Value } -end { $h }
+
+        if (-Not($dryRun -eq $True)) {
+            $null = Invoke-SqliteQuery -DataSource $database -Query $query -SqlParameters $sqlParameters -Verbose:$verbose
+        }
+        $aref = $p.ExternalId
+        $success = $true
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "CreateAccount"
+                Message = "Account created for $($account.email) ($($account.$correlationField))"
+                IsError = $false
+            })  
     }
 }
 catch {
-    $auditMessage = " not created succesfully: General error";
-    if ($verbose) {
-        $_
-    }
+    $success = $false
+    $auditLogs.Add([PSCustomObject]@{
+            Action  = "CreateAccount"
+            Message = "Failed to create account - $($_)"
+            IsError = $true
+        })
 }
 
 #build up result
 $result = [PSCustomObject]@{ 
     Success          = $success;
-    AccountReference = $accountReference;
-    AuditDetails     = $auditMessage;
+    AccountReference = $aref;
+    auditLogs        = $auditLogs;
     Account          = $account;
     
     # Optionally return data for use in other systems
-    ExportData = [PSCustomObject]@{};
+    ExportData       = [PSCustomObject]@{
+        externalID = $aref
+        email      = $account.email
+    };
 };
-
 
 Write-Output $result | ConvertTo-Json -Depth 10;
--- update.ps1
+++ update.ps1
@@ -1,47 +1,179 @@
+#####################################################
+# HelloID-Conn-Prov-Target-ProActive-SpendCloud-update
+# Version: 1.1.0
+#####################################################
+
 #Initialize default properties
 $p = $person | ConvertFrom-Json;
 $aRef = $accountReference | ConvertFrom-Json
-$config = $configuration | ConvertFrom-Json
+$config = $configuration | ConvertFrom-Json;
 
 $database = $config.database
 $verbose = $config.verbose
+$auditLogs = [Collections.Generic.List[PSCustomObject]]::new()
 
-$success = $False
-$auditMessage = "Account for person " + $p.DisplayName + " not updated succesfully"
+function GenerateMiddleName {
+    [cmdletbinding()]
+    Param (
+        [object]$person
+    )
+    try {
+        $FamilyNamePrefix = $person.Name.FamilyNamePrefix 
+        $PartnerNamePrefix = $person.Name.FamilyNamePartnerPrefix
+        $convention = $person.Name.Convention
+
+        $middlename = ""
+        switch ($convention) {
+            "B" {
+                $middlename = $FamilyNamePrefix;
+            }
+            "P" {
+                $middlename = $PartnerNamePrefix;                 
+            }
+            "BP" {
+                $middlename = $FamilyNamePrefix;
+            }
+            "PB" {
+                $middlename = $PartnerNamePrefix;   
+            }
+            Default {
+                $middlename = $FamilyNamePrefix;
+            }
+        }
+
+        return $middlename
+    }
+    catch {
+        throw("An error was found in the name convention algorithm: $($_.Exception.Message): $($_.ScriptStackTrace)")
+    
+    }
+
+}
+function GenerateLastName {
+    [cmdletbinding()]
+    Param (
+        [object]$person
+    )
+
+    try {
+        $suffix = "";
+        $givenname = if ([string]::IsNullOrEmpty($person.Name.Nickname)) { $person.Name.Initials.Substring(0, 1) }else { $person.Name.Nickname }
+        $FamilyNamePrefix = $person.Name.FamilyNamePrefix
+        $FamilyName = $person.Name.FamilyName           
+        $PartnerNamePrefix = $person.Name.FamilyNamePartnerPrefix
+        $PartnerName = $person.Name.FamilyNamePartner 
+        $convention = $person.Name.Convention
+
+        $LastName = ""
+
+        switch ($convention) {
+            "B" {
+                #  $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName  
+            }
+            "P" {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $PartnerName                    
+            }
+            "BP" {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName + " - "
+                $LastName += if (-NOT([string]::IsNullOrEmpty($PartnerNamePrefix))) { $PartnerNamePrefix + " " }
+                $LastName += $PartnerName
+            }
+            "PB" {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($PartnerNamePrefix))) { $PartnerNamePrefix + " " }
+                $LastName += $PartnerName + " - "
+                $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName
+            }
+            Default {
+                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
+                $LastName += $FamilyName
+
+            }
+        }
+            
+        return $LastName
+            
+    }
+    catch {
+        throw("An error was found in the name convention algorithm: $($_.Exception.Message): $($_.ScriptStackTrace)")
+    } 
+}
+
+
+switch ($p.details.gender) {
+    "Female" 
+    { $gender = "V" }
+    "Male" 
+    { $gender = "M" }
+    Default 
+    { $gender = "O" }
+}
 
 #Change mapping here
 $account = [PSCustomObject]@{
-    email      = $p.Accounts.MicrosoftActiveDirectory.mail;
-    voornamen  = $p.Name.NickName;
-    achternaam = $p.Custom.TOPdeskSurName;
-    adfs_login = $p.Accounts.MicrosoftActiveDirectory.SamAccountName;
-    externalId = $p.ExternalId;
+    email          = $p.Accounts.MicrosoftActiveDirectory.mail;
+    voornaam       = $p.Name.NickName
+    tussenvoegsel  = GenerateMiddleName -person $p
+    achternaam     = GenerateLastName -person $p
+    gebruikersnaam = $p.Accounts.MicrosoftActiveDirectory.samaccountname
+    externalId     = $aRef
+    geslacht       = $gender
 }
 
 Try {
-	if(-Not($dryRun -eq $True)){
-        If (Test-Path $database) {
-            Import-Module PSSQLite
-            $query = "UPDATE persons SET email = '$($account.email)', achternaam = '$($account.achternaam)', voornamen = '$($account.voornamen)', adfs_login = '$($account.adfs_login)' WHERE externalId = '$($aRef)'"
-            $null = Invoke-SqliteQuery -Query $query -DataSource $database -Verbose:$verbose
+    If (Test-Path $database) {
+        Import-Module PSSQLite     
+        $query = "UPDATE persons 
+                    SET email = @email
+                    ,achternaam = @achternaam
+                    ,voornaam = @voornaam
+                    ,tussenvoegsel = @tussenvoegsel
+                    ,gebruikersnaam = @gebruikersnaam
+                    ,geslacht = @geslacht
+                    ,createtime = datetime()
+                    WHERE externalId = @externalId"
 
-            $success = $True
-            $auditMessage = " succesfully"
-		}
+        $sqlParameters = $account.psobject.properties | ForEach-Object -begin { $h = @{} } -process { $h."$($_.Name)" = $_.Value } -end { $h }
+
+        if (-Not($dryRun -eq $True)) {
+            $null = Invoke-SqliteQuery -Query $query -DataSource $database -SqlParameters $sqlParameters -Verbose:$verbose
+        }
+        else {
+            write-warning "executing: $($query)"
+            write-warning "with parameters: $($sqlParameters | convertto-json)"
+        }
+          
+        $success = $true
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "UpdateAccount"
+                Message = "Account updated for $($account.email) ($($aref))"
+                IsError = $false
+            }) 
     }
-} catch{
-	$auditMessage = " not removed succesfully: General error"
-    Write-Verbose -Verbose "$_"
 }
-
+catch {
+    $success = $false
+    $auditLogs.Add([PSCustomObject]@{
+            Action  = "UpdateAccount"
+            Message = "Failed to update account - $($_)"
+            IsError = $true
+        })
+}
 #build up result
 $result = [PSCustomObject]@{ 
-	Success= $success;
-	AccountReference= $accountReference;
-	AuditDetails=$auditMessage;
-	Account = $account;
-	
-	# Optionally return data for use in other systems
-    ExportData = [PSCustomObject]@{};
-}
+    Success          = $success;
+    AccountReference = $aRef;
+    auditLogs        = $auditLogs;
+    Account          = $account;
+    
+    # Optionally return data for use in other systems
+    ExportData       = [PSCustomObject]@{
+        externalID = $aRef
+        email      = $account.email
+    };
+};
+
 Write-Output $result | ConvertTo-Json -Depth 10
