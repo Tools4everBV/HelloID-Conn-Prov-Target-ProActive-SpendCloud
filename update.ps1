d4a7f77225af155f9bb65a720192d19554d9355d
rscholtelubberink <80954753+rscholtelubberink@users.noreply.github.com>
2024-04-05 14:30:56 +0200

    First v2 scripts
    First versions of the V2 scripts. Mapping & SA task will follow next week

--- update.ps1
+++ update.ps1
@@ -1,179 +1,143 @@
-#####################################################
-# HelloID-Conn-Prov-Target-ProActive-SpendCloud-update
-# Version: 1.1.0
-#####################################################
+########################################
+# HelloID-Conn-Prov-Target-SpendCloudV2-Update
+#
+# Version: 2.0.0
+########################################
 
-#Initialize default properties
-$p = $person | ConvertFrom-Json;
-$aRef = $accountReference | ConvertFrom-Json
-$config = $configuration | ConvertFrom-Json;
+# Initialize default values
+$config = $actionContext.Configuration
+$outputContext.success = $false
+$outputContext.AccountReference = "DRYRUN"
 
 $database = $config.database
 $verbose = $config.verbose
-$auditLogs = [Collections.Generic.List[PSCustomObject]]::new()
 
-function GenerateMiddleName {
-    [cmdletbinding()]
-    Param (
-        [object]$person
-    )
+# Set debug logging
+switch ($verbose) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+#region functions
+
+#endregion functions
+try {
+    # Create account object from mapped data and set the correct account reference
+    $account = $actionContext.Data;
+    $person = $personContext.Person;
+
+    Write-Verbose "Verifying if DB row exists"
     try {
-        $FamilyNamePrefix = $person.Name.FamilyNamePrefix 
-        $PartnerNamePrefix = $person.Name.FamilyNamePartnerPrefix
-        $convention = $person.Name.Convention
+        ## Mooier zou zijn met aRef? 
+        $correlationField = $actionContext.CorrelationConfiguration.accountField
+        $correlationValue = $actionContext.CorrelationConfiguration.accountFieldValue
 
-        $middlename = ""
-        switch ($convention) {
-            "B" {
-                $middlename = $FamilyNamePrefix;
-            }
-            "P" {
-                $middlename = $PartnerNamePrefix;                 
-            }
-            "BP" {
-                $middlename = $FamilyNamePrefix;
-            }
-            "PB" {
-                $middlename = $PartnerNamePrefix;   
-            }
-            Default {
-                $middlename = $FamilyNamePrefix;
-            }
+        $query = "SELECT * FROM persons WHERE $correlationField = '$correlationValue'"
+        $correlationCheckResult = Invoke-SqliteQuery -Query $query -DataSource $database -Verbose:$verbose
+
+        if ($correlationCheckResult.externalId -eq $correlationValue) {
+            $currentAccount = $correlationCheckResult
         }
 
-        return $middlename
     }
     catch {
-        throw("An error was found in the name convention algorithm: $($_.Exception.Message): $($_.ScriptStackTrace)")
-    
-    }
-
-}
-function GenerateLastName {
-    [cmdletbinding()]
-    Param (
-        [object]$person
-    )
-
-    try {
-        $suffix = "";
-        $givenname = if ([string]::IsNullOrEmpty($person.Name.Nickname)) { $person.Name.Initials.Substring(0, 1) }else { $person.Name.Nickname }
-        $FamilyNamePrefix = $person.Name.FamilyNamePrefix
-        $FamilyName = $person.Name.FamilyName           
-        $PartnerNamePrefix = $person.Name.FamilyNamePartnerPrefix
-        $PartnerName = $person.Name.FamilyNamePartner 
-        $convention = $person.Name.Convention
-
-        $LastName = ""
-
-        switch ($convention) {
-            "B" {
-                #  $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
-                $LastName += $FamilyName  
-            }
-            "P" {
-                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
-                $LastName += $PartnerName                    
-            }
-            "BP" {
-                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
-                $LastName += $FamilyName + " - "
-                $LastName += if (-NOT([string]::IsNullOrEmpty($PartnerNamePrefix))) { $PartnerNamePrefix + " " }
-                $LastName += $PartnerName
-            }
-            "PB" {
-                #   $LastName += if (-NOT([string]::IsNullOrEmpty($PartnerNamePrefix))) { $PartnerNamePrefix + " " }
-                $LastName += $PartnerName + " - "
-                $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
-                $LastName += $FamilyName
-            }
-            Default {
-                #   $LastName += if (-NOT([string]::IsNullOrEmpty($FamilyNamePrefix))) { $FamilyNamePrefix + " " }
-                $LastName += $FamilyName
-
-            }
-        }
-            
-        return $LastName
-            
-    }
-    catch {
-        throw("An error was found in the name convention algorithm: $($_.Exception.Message): $($_.ScriptStackTrace)")
-    } 
-}
-
-
-switch ($p.details.gender) {
-    "Female" 
-    { $gender = "V" }
-    "Male" 
-    { $gender = "M" }
-    Default 
-    { $gender = "O" }
-}
-
-#Change mapping here
-$account = [PSCustomObject]@{
-    email          = $p.Accounts.MicrosoftActiveDirectory.mail;
-    voornaam       = $p.Name.NickName
-    tussenvoegsel  = GenerateMiddleName -person $p
-    achternaam     = GenerateLastName -person $p
-    gebruikersnaam = $p.Accounts.MicrosoftActiveDirectory.samaccountname
-    externalId     = $aRef
-    geslacht       = $gender
-}
-
-Try {
-    If (Test-Path $database) {
-        Import-Module PSSQLite     
-        $query = "UPDATE persons 
-                    SET email = @email
-                    ,achternaam = @achternaam
-                    ,voornaam = @voornaam
-                    ,tussenvoegsel = @tussenvoegsel
-                    ,gebruikersnaam = @gebruikersnaam
-                    ,geslacht = @geslacht
-                    ,createtime = datetime()
-                    WHERE externalId = @externalId"
-
-        $sqlParameters = $account.psobject.properties | ForEach-Object -begin { $h = @{} } -process { $h."$($_.Name)" = $_.Value } -end { $h }
-
-        if (-Not($dryRun -eq $True)) {
-            $null = Invoke-SqliteQuery -Query $query -DataSource $database -SqlParameters $sqlParameters -Verbose:$verbose
-        }
-        else {
-            write-warning "executing: $($query)"
-            write-warning "with parameters: $($sqlParameters | convertto-json)"
-        }
-          
-        $success = $true
-        $auditLogs.Add([PSCustomObject]@{
-                Action  = "UpdateAccount"
-                Message = "Account updated for $($account.email) ($($aref))"
+        write-error "$($_)"
+        $ex = $PSItem
+        
+        $auditMessage = "Error querying data from SQL Lite DB. Error: $($ex.Exception.Message)"
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+        
+        $outputContext.AuditLogs.Add([PSCustomObject]@{
+                Action  = "CreateAccount" # Optionally specify a different action for this audit log
+                Message = $auditMessage
                 IsError = $false
-            }) 
+            })
+        throw "Error querying data from SQL Lite DB"
     }
+
+    $previousAccount = [PSCustomObject]@{
+        achternaam     = $currentAccount.achternaam
+        tussenvoegsel  = $currentAccount.tussenvoegsel
+        voornaam       = $currentAccount.voornaam
+        gebruikersnaam = $currentAccount.gebruikersnaam
+        gender         = $currentAccount.gender
+        externalId     = $currentAccount.externalId
+        email          = $currentAccount.email
+    }
+
+
+    # Calculate changes between current data and provided data
+    $splatCompareProperties = @{
+        ReferenceObject  = @($previousAccount.PSObject.Properties) # Only select the properties to update
+        DifferenceObject = @($account.PSObject.Properties) # Only select the properties to update
+    }
+    $changedProperties = $null
+    $changedProperties = (Compare-Object @splatCompareProperties -PassThru)
+    $oldProperties = $changedProperties.Where( { $_.SideIndicator -eq '<=' })
+    $newProperties = $changedProperties.Where( { $_.SideIndicator -eq '=>' })
+
+    if (($newProperties | Measure-Object).Count -ge 1) {
+
+        If (Test-Path $database) {
+            Import-Module PSSQLite     
+            $query = "UPDATE persons 
+                        SET email = @email
+                        ,achternaam = @achternaam
+                        ,voornaam = @voornaam
+                        ,tussenvoegsel = @tussenvoegsel
+                        ,gebruikersnaam = @gebruikersnaam
+                        ,geslacht = @geslacht
+                        ,createtime = datetime()
+                        WHERE externalId = @externalId"
+    
+            $sqlParameters = $account.psobject.properties | ForEach-Object -begin { $h = @{} } -process { $h."$($_.Name)" = $_.Value } -end { $h }
+    
+            if (-Not($actionContext.DryRun -eq $true)) { 
+                $null = Invoke-SqliteQuery -Query $query -DataSource $database -SqlParameters $sqlParameters -Verbose:$verbose
+            }
+            else {
+                Write-warning "Would send: $query" 
+            }
+              
+            
+            $outputContext.AuditLogs.Add([PSCustomObject]@{
+                    Action  = "UpdateAccount" # Optionally specify a different action for this audit log
+                    Message = "Account with username $($account.userName) updated"
+                    IsError = $false
+                })
+        }
+        
+    }
+    else {
+        Write-Verbose "No Updates for SQLLite row with accountReference: [$($actionContext.References.Account)]"
+        $outputContext.AuditLogs.Add([PSCustomObject]@{
+                Action  = "UpdateAccount" # Optionally specify a different action for this audit log
+                Message = "Account with username $($account.userName) has no updates"
+                IsError = $false
+            })
+    }  
 }
 catch {
-    $success = $false
-    $auditLogs.Add([PSCustomObject]@{
-            Action  = "UpdateAccount"
-            Message = "Failed to update account - $($_)"
-            IsError = $true
-        })
-}
-#build up result
-$result = [PSCustomObject]@{ 
-    Success          = $success;
-    AccountReference = $aRef;
-    auditLogs        = $auditLogs;
-    Account          = $account;
-    
-    # Optionally return data for use in other systems
-    ExportData       = [PSCustomObject]@{
-        externalID = $aRef
-        email      = $account.email
-    };
-};
+    write-error "$($_)"
+    $ex = $PSItem
+          
+    $auditMessage = "Error adding data to SQL Lite DB. Error: $($ex.Exception.Message)"
+    Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
 
-Write-Output $result | ConvertTo-Json -Depth 10
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Action  = "UpdateAccount" # Optionally specify a different action for this audit log
+            Message = $auditMessage
+            IsError = $false
+        })
+    throw "Error adding data to SQL Lite DB"
+}
+
+finally {
+    # Check if auditLogs contains errors, if no errors are found, set success to true
+    if (-NOT($outputContext.AuditLogs.IsError -contains $true)) {
+        $outputContext.Success = $true
+    }
+
+    $outputContext.Data = $account 
+    $outputContext.PreviousData = $previousAccount
+}
